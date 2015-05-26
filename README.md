825dc7dd3e44d19df5e3812714833b4b65b8dedb
Domenico Luciani <domenicoleoneluciani@gmail.com>
2015-05-26 21:23:31 +0200

    Detect real RPIs on the network. Close #1

--- README.md
+++ README.md
@@ -4,14 +4,11 @@ A simple tool to find my raspberry pi on my LAN
 ## Usage
 ```
 > ./whereismypi
-192.168.1.116 is UP
+Your Raspberry Pi is up on: 192.168.1.116
 ```
 
-## Details
-My raspberry pi has listen ssh server on port 22 so I just try to connect to it using that port in my LAN, that's all!
-
-## TODO
-* Retrieve MAC Address, if it start with `B8:27:EB` is a raspberry pi.
+## How it works
+Scans my subnet, when it finds an host up the software searchs in the ARP table on your machine about it, if the MAC Address contains `b8:27:eb` it's own by your Raspberry Pi and returns the IP address.
 
 ## Author
 * Domenico Luciani
--- whereismypi.go
+++ whereismypi.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net"
+	"os"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -11,22 +15,37 @@ import (
 func main() {
 
 	var w sync.WaitGroup
+	found := false
 
-	for i := 1; i < 255; i++ {
+	for i := 1; i < 255 && found != true; i++ {
 		w.Add(1)
 		//Scan my subnet 192.168.1.[1-255]
-		go scan("192.168.1."+strconv.Itoa(i), 22, &w)
+		go scan("192.168.1."+strconv.Itoa(i), 22, &w, &found)
 	}
 	w.Wait()
 }
 
-func scan(host string, port int, w *sync.WaitGroup) {
+func scan(host string, port int, w *sync.WaitGroup, found *bool) {
 	portString := strconv.Itoa(port)
 	timeD, _ := time.ParseDuration("500ms")
 	conn, err := net.DialTimeout("tcp", host+":"+portString, timeD)
 	if err == nil {
 		conn.Close()
-		fmt.Printf("%s is UP\n", host)
+		table, err := os.Open("/proc/net/arp")
+		defer table.Close()
+		if err != nil {
+			log.Panic("Impossible to open arp table on your machine")
+		}
+		//Read the file line by line
+		scanner := bufio.NewScanner(table)
+		scanner.Split(bufio.ScanLines)
+		for scanner.Scan() && *found != true {
+			fields := strings.Fields(scanner.Text())
+			if fields[2] == "0x2" && strings.Contains(fields[3], "b8:27:eb:") {
+				fmt.Printf("You Raspberry Pi is up on: %s\n", host)
+				*found = true
+			}
+		}
 	}
 	(*w).Done()
 }
